9fa737fda6a27e2be1239723c0f0c1e13621245e
Kate Kraynak <kkraynak@fau.edu>
2022-08-09 23:59:32 -0400

    basic setup

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,51 +1,42 @@
 <script lang="ts" setup>
-import {send, receive, invoke} from './electron';
-
-import {ref} from 'vue';
-
-const installed = ref(false);
-const stage = ref('install');
-const downloads = ref<{
-  [key:string]: number
-}>({});
-
-send('start', '');
-receive('installed', (payload: any) => {
-  console.log('installed', payload);
-  installed.value = payload;
-  if(installed.value) {
-    stage.value = 'hash';
-  }
-});
-
-receive('download:progress', (filename: string, offset: number) => {
-  downloads.value[filename] = offset;
-  stage.value = 'download';
-})
-receive('download:error', (filename: string) => {
-  console.log('download:error', filename);
-});
-receive('download:complete', (filename: string) => {
-  console.log('download:complete', filename);
-});
-
-receive('extract:done', (filename: string, verified: boolean) => {
-  console.log(`extract:done ${filename} ${verified}`);
-});
-receive('update-lobby:done', () => {
-  console.log('update-lobby:done');
-})
+import Latest from './components/Latest.vue';
 </script>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HashChecking from './components/HashChecking.vue';
-import DownloadProgress from './components/DownloadProgress.vue';
 
 export default defineComponent({
+  data() {
+    return {
+      port: '',
+      response: [],
+      errorMessage: ''
+    }
+  },
+  methods: {
+    sendRequest() {
+      if(this.port){
+        fetch("http://localhost:" + this.port + '/latest')
+          .then(async response => {
+            const data = await response.json();
+
+            if (!response.ok) {
+              const error = (data && data.message) || response.statusText;
+              return Promise.reject(error);
+            }
+
+            this.response = data;
+          })
+          .catch(error => {
+            this.errorMessage = error;
+            console.error("There was an error!", error);
+          });
+      }
+    }
+  },
+
   components: {
-    HashChecking,
-    DownloadProgress
+    Latest
   },
 })
 </script>
@@ -53,18 +44,13 @@ export default defineComponent({
 
 <template>
   <div id="app">
-    <div>{{stage}}</div>
-    <div v-if="stage==='install'">
-      Not installed yet, find a folder to install 
-      <button @click="invoke('dialog:openDirectory', '')">Open</button>
+    <h1>Get Latest Values</h1>
+    <input v-model="port" placeholder="Enter Port" />
+    <input id="sendButton" type="submit" value="search" @click="sendRequest()">
+    <div v-if="response">
+      <h2>Response:</h2>
+      <p>{{ response }}</p>
     </div>
-    <div v-else>
-      <HashChecking />
-      <div class="downloads">
-        <div v-for="[filename, offset] in Object.entries(downloads)" :key="filename">
-          <DownloadProgress :filename="filename" :offset="offset" />
-        </div>
-      </div>
-    </div>
+    <Latest />
   </div>
 </template>
---
+++ src/renderer/components/Latest.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { NSpace } from 'naive-ui'
+
+export default defineComponent({
+    data() {
+        return {
+            message: ''
+        }
+    },
+})
+</script>
+
+
+<template>
+    {{ message }}
+</template>
